b314acfd2bd734b20bf7bfe8d0b05f4b396a2fba
danish <danish.shaikh@speedboxapp.com>
2019-06-22 20:52:42 +0530

    monitoring changes in shouldComponentUpdate

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    let state = this.state || {};
+    let op = {};
+    Object.keys(state).map(key =>
+      state[key] === nextState[key] ? undefined : (op[key] = nextState[key])
+    );
+    console.log(op);
+    return true;
+  }
   change = e => {
     let { name, value } = e.target;
     this.setState({ [name]: value });
   };
+  onBtnClick = e => {
+    this.setState({});
+  };
+
   render() {
     let { fname = "", lname = "" } = this.state || {};
-    console.log(`rendering \nfname ${fname} \nlname ${lname}`);
+    console.log(`Rendering`);
     return (
       <div>
         <input
@@ -25,6 +38,7 @@ class App extends Component {
           placeholder="lname"
           onChange={this.change}
         />
+        <input type="button" value="clickMe" onClick={this.onBtnClick} />
       </div>
     );
   }
